57ca2063bbf5f3056570c392659b1d15299c00b0
Deyvisson Eduardo <deyvissoneduardo22@gmail.com>
2020-01-25 02:43:17 -0300

    Condicional If Then Else

---
+++ show.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#Nome: ./show
+
+#Mensagem de boas vindas
+echo "Bem Vindo ao Show $0."
+
+#Coletando dados do cliente
+echo "Qual seu nome?"
+read NOME
+echo "Qual sua idade?"
+read IDADE 
+
+#Fazendo teste com if then else 
+if [[ ${IDADE} -ge 18 ]]
+
+#Se for verdade 
+then
+	echo "Sr.(a) ${NOME} ja tem mais de 18, esta liberado"
+	echo "pode entra no show"
+
+#Se for falso
+else
+	echo "Sr.(a) ${NOME} infelizmento ainda não tem 18 anos"
+	echo "não tem permisao para o show"
+
+#Fechando o IF
+fi
