d7dfd025155bd6470d9790ebb56953f6758aae08
deyvisson eduardo <deyvissoneduardo22@gmail.com>
2020-10-16 23:26:35 -0300

    convert in folder

--- convert_jpeg_to_png.sh
+++ convert_jpeg_to_png.sh
@@ -1,10 +1,48 @@
 #!/bin/bash
 
-# path
-PATH_IMAGE = ~/Downloads/imagens-livros
+#criando a funcao
+convert_image(){
+  # abre a pasta
+  cd ~/Downloads/imagens-livro
+
+  # verifica se existe a pastas de saida
+  if [ ! -d png ]
+  then 
+	mkdir png
+  fi
+
+  # for em todos arquivos de extensão .jpg
+  for image in *.jpg
+  do
+	#remove a extensao .jpg para conversao
+	local result_image=$(ls $image | awk -F. '{ print $1 }')
+	convert $result_image.jpg png/$result_image.png
+  done
+}
 
 
-for image in $@
-do
-	convert $PATH_IMAGE/$image.jpg $PAHT_IMAGE/$image.png
-done
+# cria pasta para saida de erros
+if [ ! -d erro_log ]
+then
+	mkdir erro_log
+fi
+
+# executa a funcao
+convert_image erro_log/2>erros_convert.txt
+
+
+# exibe mensagem de acordo com o status da execucao
+if [ $? -eq 0 ]
+then 
+	echo "
+***********************************
+* conversão realizada com sucesso *
+***********************************"
+      else
+
+	echo "
+***********************************
+* houve uma falha no processo     *
+***********************************"
+fi
+
